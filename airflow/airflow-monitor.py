7eaaf3e94e9e450af2f95e0d820116972a4af1ef
Azat <you@example.com>
2026-08-03 15:55:25 +0300

    add airflow change lib for dag

--- airflow/airflow-monitor.py
+++ airflow/airflow-monitor.py
@@ -1,4 +1,4 @@
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from datetime import datetime
 from airflow import DAG
 
